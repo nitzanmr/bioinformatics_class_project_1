647c3415dbc94401fe35490c813e56ae05394db9
nitzanmr <81086233+nitzanmr@users.noreply.github.com>
2024-01-18 18:05:33 +0200

    finshed homework without documentation

--- class_project_1.py
+++ class_project_1.py
@@ -1,18 +1,40 @@
-def is_valid_dna(seq : str):
+from collections import Counter
+
+gencode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+
+
+
+def is_valid_dna(seq : str) -> bool:
     seq = seq.lower()
     for i in seq:
         if i not in ['a', 't', 'c', 'g']:
             return False
     return True
 
-def get_gc_content(seq : str):
+def get_gc_content(seq : str) -> int:
     if is_valid_dna(seq) == False:
         return -1
     seq = seq.lower()
     number_of_gc = seq.count("g") + seq.count("c")
     return number_of_gc / seq.__len__() * 100
 
-def reverse_compliment(seq : str):
+def reverse_compliment(seq : str) -> str:
     if is_valid_dna(seq) == False: # if seq is not a valid dna string returns empty string
         return ""
     seq = seq.upper()
@@ -28,20 +50,62 @@ def reverse_compliment(seq : str):
             returned_seq = f'C{returned_seq}'
     return returned_seq # return the reverse compliment of the sequence in caps.
 
-def change_t_to_h(seq : str):
+def change_t_to_u(seq : str) -> str:
     seq = seq.upper()
-    seq = seq.replace("T","H")
+    seq = seq.replace("T","U")
     return seq
 
-def get_transcription(seq : str, strand: int):
+def get_transcription(seq : str, strand: int) -> str:
     if strand == -1:
         seq = reverse_compliment(seq)
-    return change_t_to_h(seq)
+    return change_t_to_u(seq)
     
-    
-    
+def stats_amino_acids() -> dict:
+    returned_dict : dict = {} 
+    for value in gencode:
+        if gencode[value] == '_':
+            gencode[value] = "END"
+        if gencode[value] in returned_dict:
+            returned_dict[gencode[value]] = returned_dict[gencode[value]] + 1
+        else:
+            returned_dict[gencode[value]] = 1
+            
+    return returned_dict
+
+def translate_seq(seq : str ,reading_frame : int = None ) -> list or str:
+    seq = seq.upper()
+    arrayed_seq = [seq[i:i+3] for i in range(0, len(seq), 3)]
+    amino_acids = []
+    print(arrayed_seq)
+    if is_valid_dna(seq) == False:
+        return ""
+    if reading_frame == None:
+        
+        for cur_seq in arrayed_seq:
+            print(cur_seq)
+            amino_acids.append(gencode[cur_seq])
+        return amino_acids
+    else:
+        return gencode[arrayed_seq[reading_frame-1]]
+
+def count_codons(seq : str) -> dict:
+    seq = seq.upper()
+    arrayed_seq = [seq[i:i+3] for i in range(0, len(seq), 3)]
+    sumerized_dict = {}
+    for i in arrayed_seq:
+        if i in sumerized_dict:
+            sumerized_dict[i] = sumerized_dict[i] + 1
+        else:
+            sumerized_dict[i] = 1
+    return sumerized_dict
+
+
+# print(gencode['AAA'])
+print(stats_amino_acids()) # TODO send an email to check the stop by END if it is a "_" and that the question is not clear on what to do. 
 seq = "ACGTTG"
 seq = seq.lower()
 print(get_gc_content(seq))
 print(reverse_compliment(seq))
 print(get_transcription(seq, -1))
+print(translate_seq(seq))
+print(count_codons(seq))
